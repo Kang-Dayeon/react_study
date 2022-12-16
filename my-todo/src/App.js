ae92dc0c962bfa2d421e4d772efd838db11a3eee
kang-dayeon <dksldiss89@gmail.com>
2022-12-17 00:24:16 +0900

    todo app 숙제

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,97 +1,48 @@
 import './App.css';
-import { useState, useRef, useCallback, cn } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'testtest',
-    },
-    {
-      id: 2,
-      text: 'testtest22222222',
-    },
-    {
-      id: 3,
-      text: 'testtest3333333333',
-    }
-  ]) 
-  const nextId = useRef(4)
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId,
-        text,
-      }
-      setTodos(todos.concat(todo))
-      // concat : 인자로 주어진 배열이나 값들을 기존 배열에 합쳐서 새 배열 반환
-      nextId.current++
-    },
-    [todos]
-  )
+  // 1. 인풋박스에서 입력후 엔터치면 화면에 뿌려주기
+  const [todos, setTodos] = useState([]) 
+  const [id, setId] = useState(0)
+  const [inputValue, setInputValue] = useState([])
+  const onClick = (e) => {
+    setInputValue('')
+    e.preventDefault()
+    setId(id + 1)
+    todos.push({
+          id : id,
+          text: inputValue,
+        })
+  }
+  // useEffect(()=>{
+  //   console.log(todos)
+  //   return () => {
+  //     console.log(inputValue);
+  //   }
+  // }, [todos])
   return (
     <div className="App">
       <h1 className="title">Todo List</h1>
-      <TodoInsert onInsert={onInsert}/>
-      <TodoList
-        todos = {todos}
-      >
-        <TodoItem />
-      </TodoList>
+      <form className="todo-form">
+        <input
+        value={inputValue}
+        placeholder="Todo List를 작성 해 보세요."
+        onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={onClick}>Add</button>
+      </form>
+      <ul className="list-wrap">
+        {
+          todos.map((todo) => {
+            return <li className="todo-list"><span>{todo.id}</span>{todo.text}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
 
-const TodoList = ({todos}) => {
-  return(
-    <ul className="list-wrap">
-      {
-        todos.map((todo) => {
-          <TodoItem
-          todo = {todo}
-          key = {todo.id}
-          />
-        })
-      }
-    </ul>
-  )
-}
-const TodoItem = ({todo}) => {
-  const {id, text} = todo;
-  return(
-    <li className="todo-list">
-        <div className="text">{id}:{text}</div>
-    </li>
-  )
-}
-const TodoInsert = ({onInsert}) => {
-  const [value, setValue] = useState('')
-  const onChenge = useCallback( e =>{
-    setValue(e.target.value);
-  },[])
-  // onChenge 이벤트 : 입력창에 친 값을 추적해서 setValue에 저장
-  // useCallback : 리랜더링할때 낭비를 방지하기 위해서 전에 생성된 함수를 다시 재활용 하기위해 사용
-  const onSubmit = useCallback(
-    e => {
-      onInsert(value)
-      setValue('')
-      e.preventDefault()
-    }
-  ,[onInsert, value])
-  // todo 배열에 새 데이터를 추가하는 함수
-  return(
-    <form className="TodoInsert" onSubmit={onSubmit}>
-      <input
-      onChenge={onChenge}
-      value={value}
-      placeholder="Todo List를 작성 해 보세요."
-      />
-      <button type="submit">
-        Add
-      </button>
-    </form>
-  )
-}
 
 export default App;
