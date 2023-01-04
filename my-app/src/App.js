771001a9703e7ed91e31255c4ec9d1983af2f05e
kang-dayeon <dksldiss89@gmail.com>
2023-01-04 22:28:40 +0900

    병합

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React from 'react';
-import {Route, Link, Routes} from 'react-router-dom'
+import { Route } from 'react-router-dom';
+// import LightRoom from './component/light-room/lightRoom'
+import Home from './component/Home/nav';
 import Snow from './component/snowflake/snow';
-import SnowTwo from './component/snowflake/snow_02';
+import SnowTwo from './component/snowflake/snow';
 
  const App = () => {
 
@@ -10,18 +12,18 @@ import SnowTwo from './component/snowflake/snow_02';
      <div className='app'>
       <ul>
         <li>
-          <Link to="/snow01">Snow01</Link>
+          <link to="/">Home</link>
         </li>
         <li>
-          <Link to="/snow02">Snow02</Link>
+          <link to="/snow01">Snow01</link>
+        </li>
+        <li>
+          <link to="/snow02">Snow02</link>
         </li>
       </ul>
-
-       <Routes>
-         <Route path="/snow01" element={ <Snow /> }></Route>
-         <Route path="/snow02" element={ <SnowTwo /> }></Route>
-       </Routes>
-
+      <Route exact path="/" component={ Home }></Route>
+      <Route path="/snow01" component={ Snow }></Route>
+      <Route path="/snow02" component={ SnowTwo }></Route>
      </div>
    )
  }
