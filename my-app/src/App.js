a85423846f715d8f0a143117c7695a453a8e5788
강다연 <dksldiss89@gmail.com>
2023-01-05 18:21:35 +0900

    숙제 로그인기능

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,28 +2,29 @@ import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
 // import LightRoom from './component/light-room/lightRoom'
-import Home from './component/Home/nav';
 import Snow from './component/snowflake/snow';
 import SnowTwo from './component/snowflake/snow';
+import LoginPage from './component/login/login'
 
  const App = () => {
 
    return (
      <div className='app'>
-      <ul>
-        <li>
-          <link to="/">Home</link>
-        </li>
-        <li>
-          <link to="/snow01">Snow01</link>
-        </li>
-        <li>
-          <link to="/snow02">Snow02</link>
-        </li>
-      </ul>
-      <Route exact path="/" component={ Home }></Route>
-      <Route path="/snow01" component={ Snow }></Route>
-      <Route path="/snow02" component={ SnowTwo }></Route>
+      {/*<ul>*/}
+      {/*  <li>*/}
+      {/*    <link to="/">Home</link>*/}
+      {/*  </li>*/}
+      {/*  <li>*/}
+      {/*    <link to="/snow01">Snow01</link>*/}
+      {/*  </li>*/}
+      {/*  <li>*/}
+      {/*    <link to="/snow02">Snow02</link>*/}
+      {/*  </li>*/}
+      {/*</ul>*/}
+      {/*<Route exact path="/" component={ Home }></Route>*/}
+      {/*<Route path="/snow01" component={ Snow }></Route>*/}
+      {/*<Route path="/snow02" component={ SnowTwo }></Route>*/}
+       <LoginPage />
      </div>
    )
  }
--- my-app/src/component/login/login.js
+++ my-app/src/component/login/login.js
@@ -1,16 +1,69 @@
-import React from "react";
+import React, {useState} from 'react'
+import './login.css'
 
 const LoginPage = () => {
+  const userData = [
+    {
+      name: "정주호",
+      loginId: "meju7015",
+      password: "123123123",
+      phoneNumber: "010-2396-2036",
+      isAdmin: true,
+      accessToken: "18c1021a9b9d8e9999edf028182e3",
+      abilities: [
+        {
+          action: "dashboard",
+          permission: "manage"
+        },
+        {
+          action: "users",
+          permission: "read"
+        }
+      ]
+    },
+    {
+      name: "스틱",
+      loginId: "sticker",
+      password: "123123123",
+      phoneNumber: "010-1243-1234",
+      isAdmin: false,
+      accessToken: "43c1051a9b9d8e7779123328182e4",
+      abilities: [
+        {
+          action: "dashboard",
+          permission: "read"
+        },
+        {
+          action: "users",
+          permission: "read"
+        }
+      ]
+    }
+  ]
+  const [id, setId] = useState(null)
+  const [pw, setPw] = useState(null)
+  const onLogin = (e) => {
+    e.preventDefault()
+    const user = userData.filter(list => list.loginId === id && list.password === pw)
+    console.log(user)
+  }
+  const handleId = (e) => {
+    setId(e.target.value)
+  }
+  const handlePw = (e) => {
+    setPw(e.target.value)
+  }
     return (
-        <>
+        <div className="login-page">
+          <div className="login-page__wrap">
             <h2>Login</h2>
             <form className="login-form">
-                <label for="id">ID</label>
-                <input id="id" type="text" placeholder="아이디를 입력해주세요."></input>
-                <label for="pw">Password</label>
-                <input id="pw" type="text" placeholder="비밀번호를 입력해주세요."></input>
+              <input id="id" type="text" value={id} onChange={handleId} placeholder="아이디를 입력해주세요."></input>
+              <input id="pw" type="text" value={pw} onChange={handlePw} placeholder="비밀번호를 입력해주세요."></input>
+              <button type="submit" onClick={onLogin}>LOGIN</button>
             </form>
-        </>
+          </div>
+        </div>
     )
 }
 
